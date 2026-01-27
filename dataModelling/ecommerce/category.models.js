361b7d2863e807889af23ea4b03d2c329cc60cc0
k-ghale <kabinghale888@gmail.com>
2026-01-26 22:00:22 -0500

    ecommerce model added

---
+++ dataModelling/ecommerce/category.models.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+const categorySchema = new mongoose.Schema(
+    {
+        name : {
+            name: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Category = mongoose.model("Category" , categorySchema);
