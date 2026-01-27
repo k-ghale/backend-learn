666c1df8bd45851612a9e28ae43aab32f4235799
k-ghale <kabinghale888@gmail.com>
2026-01-26 21:29:28 -0500

    added data-modelling

---
+++ dataModelling/todos/todo.models.js
@@ -0,0 +1,32 @@
+
+import mongoose from "mongoose'
+
+
+const todoSchema = new mongoose.Schema(
+    {
+        content:{
+            type:String,
+            required: true
+        },
+        complete: {
+            type: Boolean,
+            default : true
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        subTodos:[
+            {
+                type : mongoose.Schema.Types.ObjectId,
+                ref: "subTodo",
+            }
+        ] //Array of subtodos
+    }
+    ,
+    {timestamps : true}
+);
+
+export const Todo = mongoose.model("Todo", todoSchema);
+
+
